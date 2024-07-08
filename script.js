387e46d95f087aa9b44a786639a7bd85ae81786d
harri3d <92763783+harri3d@users.noreply.github.com>
2024-07-07 19:50:47 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 const btnOpen = document.querySelector('#btnOpen');
 const btnClose = document.querySelector('#btnClose');
+const topnav__menu = document.querySelector('.topnav__menu');
 const media = window.matchMedia('(width < 40em)');
 const body = document.querySelector('body');
 
+function setuptopnav() {
+    topnav__menu.style.transition = 'none';
+}
+
 function openMobileMenu() {
+    topnav__menu.removeAttribute('style');
     btnOpen.setAttribute('aria-expanded', 'true');
     bodyScrollLockUpgrade.disableBodyScroll(body);
     btnClose.focus();
+
 }
 
 function closeMobileMenu() {
@@ -14,8 +21,13 @@ function closeMobileMenu() {
     console.log("button clicked")
     bodyScrollLockUpgrade.enableBodyScroll(body);
     btnOpen.focus();
+
+    setTimeout(() => {
+        topnav__menu.style.transition = 'none';
+    }, 500)
+
 }
 
-
+setuptopnav();
 btnOpen.addEventListener('click', openMobileMenu);
 btnClose.addEventListener('click', closeMobileMenu);
